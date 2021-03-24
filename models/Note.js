10224b06b53210407ff221ba706e387800f9ccf4
Mogakamo <mogaka.amo254@gmail.com>
2021-03-24 16:52:11 +0300

    Added the models directory

---
+++ models/Note.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require =('validator')
+
+const NoteSchema = new mongoose.Schema(
+	{
+	  title: {
+		type: String,
+		required: true,
+		validator: {
+			validator(title) {
+				return validator.isAlphanumeric(title);
+			},
+		},
+	  },
+	  description: {
+		type: String,
+		require: true,
+		validator: {
+			validator(description) {
+				return validator.isAlphanumeric(description);
+			},
+		},
+	  },
+	  reminder: {
+		type: Boolean,
+		required: false,
+		default: false,
+	  },
+	  status: {
+		type: String,
+		enum: ['completed', 'new'],
+	  }
+	}
+)
